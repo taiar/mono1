a9cec234abbf8915f9f4699fe5be1bbfd0e46eac
André Taiar <taiar@hotmail.com>
2015-11-08 19:34:05 -0200

    Removendo todos os pacotes que foram comitados.

--- dart/syncasync/sync.dart
+++ dart/syncasync/sync.dart
@@ -1,27 +1,26 @@
 import 'dart:math';
 import 'package:http/http.dart' as http;
 
-readFromRemoteFile(url, sentence) {
+readFromRemoteFile(url, sentence) async {
   var rng = new Random();
   print(sentence);
-  http.get(url).then((val){
-    var texto = val.body;
-    var frases = texto.split("\n");
-    print(frases[rng.nextInt(frases.length)]);
-  });
+  var response = await http.get(url);
+  var texto = response.body;
+  var frases = texto.split("\n");
+  print(frases[rng.nextInt(frases.length)]);
 }
 
-ramones() {
-  readFromRemoteFile("http://aurelio.net/doc/ramones.txt",
+ramones() async {
+  await readFromRemoteFile("http://aurelio.net/doc/ramones.txt",
     "Please wait, consulting the Ramones:");
 }
 
-natal() {
-  readFromRemoteFile("http://www.crossladies.com/feliz_natal.txt",
+natal() async {
+  await readFromRemoteFile("http://www.crossladies.com/feliz_natal.txt",
     "This is how we say 'Merry Christmas' in:");
 }
 
-main() {
-  ramones();
+main() async {
+  await ramones();
   natal();
 }
