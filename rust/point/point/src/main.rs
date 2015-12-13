92356c0d8f0f0e6dc0ac77fc527326f3d6b72f05
André Taiar <taiar@hotmail.com>
2015-12-13 19:30:39 -0200

    Fast Inverse Square Root.

--- rust/point/point/src/main.rs
+++ rust/point/point/src/main.rs
@@ -6,12 +6,21 @@ struct Point {
 }
 
 impl Point {
-	fn distanceFromO(&self) -> f32 {
-		f32::sqrt(self.x * self.x + self.y * self.y)
+	fn squareDistanceFromO(&self) -> f32 {
+		(self.x * self.x + self.y * self.y)
 	}
+}
+
+fn fast_inv_sqrt(x: f32) -> f32 {
+    let f: f32 = unsafe {
+        let i: i32 = std::mem::transmute(x);
+        std::mem::transmute(0x5f3759df - (i >> 1))
+    };
+     1./(f * (1.5 - 0.5 * x * f * f);)
+    (1. / ret)
 }
 
 fn main() {
     let point = Point { x: 3f32, y: 4f32 }; 
-    println!("The distance of the origin is ({})", point.distanceFromO(), );
+    println!("The distance of the origin is ({})", fast_inv_sqrt(point.squareDistanceFromO()), );
 }
